b64ae971d2a0242a26294270793336018a644407
eyar1234 <eyar2000@gmail.com>
2023-06-13 13:19:40 +0300

    classVersion

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import "./App.css";
+import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/search-box/search-box.component";
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchFild: " ",
+    };
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
+  }
+  onSearchGhange = (event) => {
+    const searchFild = event.target.value.toLowerCase();
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    this.setState(() => {
+      return { searchFild };
+    });
+  };
+  render() {
+    const { monsters, searchFild } = this.state;
+    const { onSearchGhange } = this;
+    const filterdMonster = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searchFild);
+    });
+
+    return (
+      <div className="App">
+        <h1 className="titel">my first react app</h1>
+        <SearchBox
+          onChangehendler={onSearchGhange}
+          placeholder={"search monster"}
+          className="monster search box"
+        />
+        <CardList monsters={filterdMonster} />
+      </div>
+    );
+  }
 }
 
 export default App;
